f72fcf8ef6b3cd7e0e2825ca392e16d0dbb092aa
Matthew Yap <90490472+mattyapers@users.noreply.github.com>
2024-12-25 20:36:12 +0800

    Update Welcome_to_TMDB_Movies!.py

--- Welcome_to_TMDB_Movies!.py
+++ Welcome_to_TMDB_Movies!.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(page_title="TMDB Movie Dataset", page_icon="🎬")
 st.title("Welcome to TMDB Movie Dataset Dashboard!")
 st.write("This dashboard provides insights into the movie dataset, which includes information on revenue, budget, genre, language, and more. Data obtained from https://www.kaggle.com/datasets/asaniczka/tmdb-movies-dataset-2023-930k-movies, cleaned by me!" )
-
+st.write("*New Dataset contains 897382 entries")
 st.write("Use the sidebar to navigate between pages.")
 
 
